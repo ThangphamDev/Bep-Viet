0088e08395190b106793c676eb85db2cce9c7870
viet4628 <vietnguyen4628@gmail.com>
2025-10-22 22:57:39 +0700

    [merge] complete

--- mobile/lib/data/models/recipe_model.dart
+++ mobile/lib/data/models/recipe_model.dart
@@ -139,13 +139,13 @@ class RecipeModel {
     return null;
   }
 
-
-  // static double? _parseDouble(dynamic value) {
-  //   if (value == null) return null;
-  //   if (value is double) return value;
-  //   if (value is int) return value.toDouble();
-  //   if (value is String) return double.tryParse(value);
-  //   return null;
+  static double? _parseDouble(dynamic value) {
+    if (value == null) return null;
+    if (value is double) return value;
+    if (value is int) return value.toDouble();
+    if (value is String) return double.tryParse(value);
+    return null;
+  }
 
   RecipeModel copyWith({
     String? id,
