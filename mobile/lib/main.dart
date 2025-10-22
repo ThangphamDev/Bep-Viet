573bf68d56b571f773b40184e386776c9ec3765f
hikaru2561 <nguyenkimquang2561@gmail.com>
2025-10-22 23:29:06 +0700

    chore: Remove premium features - keep only auth and delete account

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -10,11 +10,8 @@ import 'data/sources/remote/api_service.dart';
 import 'data/sources/remote/auth_service.dart';
 import 'data/sources/remote/google_auth_service.dart';
 import 'data/sources/local/biometric_auth_service.dart';
-import 'data/sources/remote/premium_service.dart';
 import 'data/repositories/auth_repository.dart';
-import 'data/repositories/premium_repository.dart';
 import 'presentation/features/auth/cubit/auth_cubit.dart';
-import 'presentation/features/premium/cubit/premium_cubit.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,25 +28,20 @@ void main() async {
     googleAuthService,
     biometricAuthService,
   );
-  final premiumService = PremiumService(dio);
-  final premiumRepository = PremiumRepository(premiumService);
 
   runApp(
     BepVietApp(
       authRepository: authRepository,
-      premiumRepository: premiumRepository,
     ),
   );
 }
 
 class BepVietApp extends StatefulWidget {
   final AuthRepository authRepository;
-  final PremiumRepository premiumRepository;
 
   const BepVietApp({
     super.key,
     required this.authRepository,
-    required this.premiumRepository,
   });
 
   @override
@@ -58,31 +50,25 @@ class BepVietApp extends StatefulWidget {
 
 class _BepVietAppState extends State<BepVietApp> {
   late final AuthCubit _authCubit;
-  late final PremiumCubit _premiumCubit;
   late final GoRouter _router;
 
   @override
   void initState() {
     super.initState();
     _authCubit = AuthCubit(widget.authRepository);
-    _premiumCubit = PremiumCubit(widget.premiumRepository);
     _router = AppRouter.router(_authCubit);
   }
 
   @override
   void dispose() {
     _authCubit.close();
-    _premiumCubit.close();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<AuthCubit>.value(value: _authCubit),
-        BlocProvider<PremiumCubit>.value(value: _premiumCubit),
-      ],
+    return BlocProvider<AuthCubit>.value(
+      value: _authCubit,
       child: MaterialApp.router(
         title: AppConfig.appName,
         debugShowCheckedModeBanner: false,
