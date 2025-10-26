3618c1c9776679690591447a9bddec8c425e819c
Thắng Phạm Xuân <thang@gmail.com>
2025-10-26 14:42:02 +0700

    update version 26 -> 27

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -9,11 +9,12 @@ plugins {
 android {
     namespace = "com.bepviet.app.mobile"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -42,6 +43,7 @@ android {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.25")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
 
 flutter {
